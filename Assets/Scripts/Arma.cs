8c0630951d34ca85c3180a36d71b85bc3c7380b6
Vinícius Pimentel <vinipcouto@gmail.com>
2016-11-11 15:00:26 -0200

    - Added FMOD integration - Implemented basic shooting sound

--- Assets/Scripts/Arma.cs
+++ Assets/Scripts/Arma.cs
@@ -8,6 +8,9 @@ public class Arma : MonoBehaviour
 	private float tempoUltimoTiro;
 	public float intervaloTiro;
 
+	[FMODUnity.EventRef]
+	public string shotFX;
+
 	void Start()
 	{
 		tempoUltimoTiro = 0;
@@ -25,6 +28,13 @@ public class Arma : MonoBehaviour
 			var bala = Instantiate(prefabBala, transform.position, transform.rotation) as Bala;
 			bala.GetComponent<Rigidbody2D>().velocity = new Vector2(0, velocidade);
 			tempoUltimoTiro = intervaloTiro;
+
+			if (!string.IsNullOrEmpty(shotFX))
+			{
+				FMOD.Studio.EventInstance e = FMODUnity.RuntimeManager.CreateInstance(shotFX);
+				e.start();
+				e.release();
+			}
 		}
 	}
 }
